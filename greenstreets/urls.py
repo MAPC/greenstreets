e2f023ad7633036c811aca6849c47e92ab21e5a8
Christian Spanring <cspanring@gmail.com>
2013-04-27 21:03:37 -0400

    removes legacy urls

--- greenstreets/urls.py
+++ greenstreets/urls.py
@@ -7,29 +7,20 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'greenstreets.views.home', name='home'),
-    # url(r'^greenstreets/', include('greenstreets.foo.urls')),
 
-    # homepage
+    # Home
     url(r'^$', TemplateView.as_view(template_name='survey/index.html'), name='home'),
     
-    # commuterform
+    # Commuterform
     url(r'^commuterform/$', 'survey.views.commuter', name='commuterform'),
-
-    # studentform
-    url(r'^studentform/$', 'survey.views.student', name='studentform'),
     
-    # schools
-    url(r'^district/(?P<slug>[-\w]+)/schools/$', 'survey.views.get_schools'),
-    
-    (r'^grappelli/', include('grappelli.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    (r'^grappelli/', include('grappelli.urls')),
 )
 
 urlpatterns += staticfiles_urlpatterns()
